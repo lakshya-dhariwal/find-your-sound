847b0d3d6e6705bb881a4fcdabd7c5eb214ce5f0
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-01-18 22:07:20 +0530

    🚀 constants update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 const data = {
   CLIENT_ID: "5e6227134d7c4e98a8023a732781b402",
-  REDIRECT_URI: "http://localhost:3000",
+  REDIRECT_URI: "https://findyoursound.vercel.app/",
   SCOPES:
     "streaming%20user-read-private%20user-library-read%20user-library-modify%20playlist-modify-public",
-  AUTH_SERVER_URL: "http://localhost:3001/",
+  AUTH_SERVER_URL: "https://spotify-auth-server-1.lakshya125.repl.co/",
 };
 export default data;
