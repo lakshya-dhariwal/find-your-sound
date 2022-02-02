f9d91f74a7402982a3e39ebd78739fda6b3ee0e1
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-02-02 17:26:46 +0530

    🐛 Fix:invalid redirect URI

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,8 +1,8 @@
 const data = {
   CLIENT_ID: "5e6227134d7c4e98a8023a732781b402",
-  REDIRECT_URI: "https://localhost:3000/",
+  REDIRECT_URI: "http://localhost:3000",
   SCOPES:
     "streaming%20user-read-private%20user-library-read%20user-library-modify%20playlist-modify-public",
-  AUTH_SERVER_URL: "https://localhost:3001/",
+  AUTH_SERVER_URL: "http://localhost:3001/",
 };
 export default data;
